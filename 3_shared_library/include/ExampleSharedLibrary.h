36aaa974398b6e332608e7fbbf5732d9eba1b602
xiehui <1059248139@qq.com>
2021-12-07 07:12:12 +0800

    [rev]对Windows兼容

--- 3_shared_library/include/ExampleSharedLibrary.h
+++ 3_shared_library/include/ExampleSharedLibrary.h
@@ -1,7 +1,21 @@
 #ifndef EXAMPLE_SHARED_LIBRARY_H
 #define EXAMPLE_SHARED_LIBRARY_H
 
-class ExampleSharedLibrary
+#ifndef EXAMPLE_DLL
+# ifdef _WIN32
+#  if defined( BUILD_DLL )
+#    define EXAMPLE_DLL __declspec(dllexport)
+#  elif defined( USE_DLL )
+#    define EXAMPLE_DLL __declspec(dllimport)
+#  else
+#    define EXAMPLE_DLL
+#  endif
+# else
+#  define EXAMPLE_DLL
+# endif
+#endif
+
+class EXAMPLE_DLL ExampleSharedLibrary
 {
 	public:
 		void print(void);
